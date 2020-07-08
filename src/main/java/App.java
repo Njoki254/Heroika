93d1b639f1a895f5cc4864d1e2a98dd2843b3681
Njoki Kamau <njokifaith61@gmail.com>
2020-07-08 14:32:18 +0300

    fix the form action error stopping handlebars from displaying correctly

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -11,83 +11,45 @@ import static spark.Spark.*;
 public class App {
     public static void main(String[] args) { //type “psvm + tab” to autocreate this
         staticFileLocation("/public");
-
-        //get: show new post form
-        get("/squads/new", (req, res) -> {
-            Map<String, Object> model = new HashMap<>();
-            return new ModelAndView(model, "models.Squad-Form.hbs");
+        //Display homepage
+        get("/", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            return new ModelAndView(model, "Homepage.hbs");
+        }, new HandlebarsTemplateEngine());
+        get("/profiles", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            return new ModelAndView(model, "Heroes-Profile.hbs");
+        }, new HandlebarsTemplateEngine());
+        get("/squad-form", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            return new ModelAndView(model, "Squad-Form.hbs");
+        }, new HandlebarsTemplateEngine());
+        get("/Hero-Details", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            return new ModelAndView(model, "Hero-Details.hbs");
         }, new HandlebarsTemplateEngine());
 
+        //Code to retrieve user's input
+        get("/success", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            String squadName = request.queryParams("squadName");
+            String squadCause = request.queryParams("squadCause");
+
+            String members = request.queryParams("members");
+            model.put("squadName", squadName);
+            model.put("squadCause", squadCause);
+            model.put("members", members);
+            return new ModelAndView(model, "Success.hbs");
+        }, new HandlebarsTemplateEngine());
         //post: process new post form
-        post("/squads/new", (req, res) -> { //URL to make new post on POST route
+        post("/success", (req, res) -> { //URL to make new post on POST route
             Map<String, Object> model = new HashMap<>();
 
 
             String content = req.queryParams("content");
             Squad newSquad= new Squad(content);
             model.put("squad", newSquad);
-            return new ModelAndView(model, "success.hbs");
-        }, new HandlebarsTemplateEngine());
-
-        //get: show all posts
-        get("/", (req, res) -> {
-            Map<String, Object> model = new HashMap<>();
-            ArrayList<Squad> squads = Squad.getAll();
-            model.put("squads",squads);
-
-            return new ModelAndView(model, "homepage.hbs");
-        }, new HandlebarsTemplateEngine());
-
-        //get: show an individual post
-        get("/squads/:id", (req, res) -> {
-            Map<String, Object> model = new HashMap<>();
-            int idOfSquadToFind = Integer.parseInt(req.params("id")); //pull id - must match route segment
-            Squad foundSquad = Squad.findById(idOfSquadToFind); //use it to find post
-            model.put("squad", foundSquad); //add it to model for template to display
-            return new ModelAndView(model, "models.Squad-Details.hbs"); //individual post page.
-        }, new HandlebarsTemplateEngine());
-
-        //get: show a form to update a post
-        get("/squads/:id/update", (req, res) -> {
-            Map<String, Object> model = new HashMap<>();
-            int idOfSquadToEdit = Integer.parseInt(req.params("id"));
-            Squad editSquad = Squad.findById(idOfSquadToEdit);
-            model.put("editSquad", editSquad);
-            return new ModelAndView(model, "models.Squad-Form.hbs");
-        }, new HandlebarsTemplateEngine());
-
-        //post: process a form to update a post
-        post("/suads/:id/update", (req, res) -> { //URL to make new post on POST route
-            Map<String, Object> model = new HashMap<>();
-            String newContent = req.queryParams("content");
-            int idOfSquadToEdit = Integer.parseInt(req.params("id"));
-            Squad editSquad = Squad.findById(idOfSquadToEdit);
-            editSquad.update(newContent);
-            return new ModelAndView(model, "success.hbs");
-        }, new HandlebarsTemplateEngine());
-
-        //get: delete an individual post
-        get("/squads/:id/delete", (req, res) -> {
-            Map<String, Object> model = new HashMap<>();
-            int idOfSquadToDelete = Integer.parseInt(req.params("id")); //pull id - must match route segment
-            Squad deleteSquad = Squad.findById(idOfSquadToDelete); //use it to find post
-            deleteSquad.deleteSquad();
-            return new ModelAndView(model, "success.hbs");
-        }, new HandlebarsTemplateEngine());
-
-        //get: delete all posts
-        get("/squads/delete", (req, res) -> {
-            Map<String, Object> model = new HashMap<>();
-            Squad.clearAllSquads();
-            return new ModelAndView(model, "success.hbs");
-        }, new HandlebarsTemplateEngine());
-        get("/models.Squad-Form", (request, response) -> {
-            Map<String, Object> model = new HashMap<String, Object>();
-            return new ModelAndView(model, "models.Squad-Form.hbs");
-        }, new HandlebarsTemplateEngine());
-        get("/profiles", (request, response) -> {
-            Map<String, Object> model = new HashMap<String, Object>();
-            return new ModelAndView(model, "Heroes-Profile.hbs");
+            return new ModelAndView(model, "Success.hbs");
         }, new HandlebarsTemplateEngine());
     }
 
--- src/main/java/Hero.java
+++ src/main/java/Hero.java
@@ -1,2 +1,39 @@
+
+import java.util.ArrayList;
+
 public class Hero {
+    private String content;
+    private static ArrayList<Hero> instances = new ArrayList<>();
+    private boolean published; //i’m new
+    private int id;
+
+    public Hero (String content){
+        this.content = content;
+        this.published = false; //also new
+        instances.add(this);
+        this.id = instances.size();
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public static ArrayList<Hero> getAll(){
+        return instances;
+    }
+
+    public static void clearAllSquads(){
+        instances.clear(); //clear as a method is part of the ArrayList class.
+    }
+
+    public boolean getPublished(){ //new too
+        return this.published;
+    }
+    public int getId() {
+        return id;
+    }
+    public static Hero findById(int id){
+        return instances.get(id-1); //why minus 1? See if you can figure it out.
+    }
 }
+
