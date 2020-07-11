79823db394ddda4ead382ea503be588617c9e7b3
Njoki Kamau <njokifaith61@gmail.com>
2020-07-11 16:43:42 +0300

    removed postgres dao folders to remove errors

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,7 +2,6 @@
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
--- src/test/java/SquadTest.java
+++ src/test/java/SquadTest.java
@@ -2,7 +2,6 @@
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
-import org.junit.*;
 import static org.junit.Assert.*;
 
 public class SquadTest {
